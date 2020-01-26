4b8c17203d6b8e0b8dc2552af55ae95ad28f3254
Gaurav Chalana <41099402+gauravchalana1@users.noreply.github.com>
2020-01-26 16:25:08 -0600

    Update star.java

--- star.java
+++ star.java
@@ -1,18 +1,20 @@
 package learningApp;
 
-public class star {
+public class rightStar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 0;
-		while(i<10) {
-			if(i%2 == 0) {
-				System.out.println("Hello World");
-			} else {
-				System.out.println("Jurrasic World");
+		int i, j, z;
+		int n = 5;
+		for (i = 0; i <= n; i++) {
+			for(j = n - 1; j >= i; j--) {
+				System.out.print(" ");
 			}
-			
-			i++;
+			System.out.print("*");
+			for(z = 0; z <= j; z++) {
+				System.out.print("*");
+			}
+			System.out.println("");
 		}
 	}
 
